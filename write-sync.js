3c59ad48ad20e01e940aad386c4fcc70feecb5ab
MTofayelAhmed <tofayelahmed324@gmail.com>
2026-02-15 22:50:25 +0100

    done with this write async

--- write-sync.js
+++ write-sync.js
@@ -1,5 +1,19 @@
 const fs = require('fs');
 const content1= " this is the first line of the file.\n nodejs is a great platform for building server-side applications.\n";
+const contentasync = " this is the second line of the file.\n nodejs has a rich ecosystem of libraries and frameworks.\n";
+
+
 
 fs.writeFileSync("./output/writesync.txt",  content1, "utf-8");
 console.log("File written successfully");
+
+
+fs.writeFile("./output/writeasync.txt", contentasync, "utf-8", (err) => {
+
+    if (err) {
+        console.error("Error writing file:", err);
+    } else {
+        console.log("File written successfully");
+    }})
+
+    
