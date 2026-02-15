6ee87413c43ee9e1357e906a78981318cf6da761
MTofayelAhmed <tofayelahmed324@gmail.com>
2026-02-15 22:06:40 +0100

    done with basic greeing arg

--- greeting.js
+++ greeting.js
@@ -1,1 +1,17 @@
-const name = arg[2] || "world";
+const args= process.argv;
+
+const name = args[2] || "world";
+
+let greeting;
+
+  let time = new Date().getHours();
+
+ if(time<12){
+    greeting = "Good morning";
+ } else if(time<18){
+    greeting = "Good afternoon";
+ } else {
+    greeting = "Good evening";
+ }
+ console.log(`${greeting}, ${name}`);
+ 
