584578d4abc40226615c643ffcb3a2f883d847bd
MTofayelAhmed <tofayelahmed324@gmail.com>
2026-02-19 00:45:42 +0100

    done with os basic nodejs

--- os-basic.js
+++ os-basic.js
@@ -1,5 +1,20 @@
 const os = require("os");
 console.log("operating system: 0", os.platform());
+
 console.log("operating system: 1", os.type());
 console.log("operating system: 2", os.release());
 console.log("operating system: 3", os.arch());
+console.log("operating system: 4", os.cpus());
+const cpus= os.cpus();
+
+console.log("operating system: 5", cpus[0].model);
+console.log("operating system: 5", cpus[0].length)
+console.log("operating system: 6", cpus[0].speed);
+
+const totalMemory = os.totalmem();
+const freeMemory = os.freemem();
+const upTime = os.uptime();
+
+console.log("total memory: ", totalMemory);
+console.log("free memory: ", freeMemory);
+console.log("up time: ", upTime);
