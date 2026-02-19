c2dfb79781d1d904c2b636ba11d905523dfb9814
MTofayelAhmed <tofayelahmed324@gmail.com>
2026-02-19 01:12:51 +0100

    done with hashing basic with crypto modulen

--- hashing.js
+++ hashing.js
@@ -3,3 +3,10 @@ console.log("\n MD5: Hash: ");
 const hash1= crypto.createHash("md5").update("Hello World").digest("hex");
 console.log(hash1);
 // not recommended for security purposes
+
+const sha256Hash = crypto.createHash("sha256").update("Hello World").digest("hex");
+console.log("\n SHA256 Hash: ", sha256Hash);
+
+
+const sha512Hash = crypto.createHash("sha512").update("Hello World").digest("hex");
+console.log("\n SHA512 Hash: ", sha512Hash);
